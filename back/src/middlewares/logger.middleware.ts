0a04262a9cf76ce7c65bfe267d215f6354475464
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2023-11-22 12:21:46 +0100

    maj du logger: plus propre

--- back/src/middlewares/logger.middleware.ts
+++ back/src/middlewares/logger.middleware.ts
@@ -4,12 +4,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 const getProcessingTimeInMS = (time: [number, number]): string => {
-  return `${(time[0] * 1000 + time[1] / 1e6).toFixed(2)}ms`
+  return `${(time[0] * 1000 + time[1] / 1e6).toFixed(2)}ms`;
 }
 
 /**
  * add logs for an API endpoint using the following pattern
- *  [id][timestamp] method:url START processing
+ *  [id][timestamp] method:url START
  *  [id][timestamp] method:url response.statusCode END processing
  *
  * @param req Express.Request
@@ -18,7 +18,7 @@ const getProcessingTimeInMS = (time: [number, number]): string => {
  */
 const logger = (req: Request, res: Response, next: NextFunction) => {
   // generate unique identifier
-  const id = uuidv4();
+  const id: string = uuidv4();
 
   // get timestamp
   const timestamp: string = moment(new Date()).format("DD-MM-YYYY HH:mm:ss");
@@ -27,18 +27,17 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   const { method, url } = req;
 
   // log start of the execution process
-  const start = process.hrtime();
-  const startText =  `START:${getProcessingTimeInMS(start)}`
-  const idText = `[${id}]`;
-  const timeStampText =  `[${timestamp}]`;
-  console.log(`${idText}${timeStampText} ${method}:${url} ${startText}`);
+  const start: [number, number] = process.hrtime();
+  const text_id: string = `[${id}]`;
+  const time_stamp_text: string =  `[${timestamp}]`;
+  console.log(`${text_id}${time_stamp_text} ${method}:${url} START`);
 
   // trigger once a response is sent to the client
   res.once('finish', () => {
     // log end of the execution process
-    const end = process.hrtime(start);
-    const endText =  `END:${getProcessingTimeInMS(end)}`;
-    console.log(`${idText}${timeStampText} ${method}:${url} ${res.statusCode} ${endText}`);
+    const end: [number, number] = process.hrtime(start);
+    const end_text: string = `END:${getProcessingTimeInMS(end)}`;
+    console.log(`${text_id}${time_stamp_text} ${method}:${url} ${res.statusCode} ${end_text}`);
   });
 
   // execute next middleware/event handler
