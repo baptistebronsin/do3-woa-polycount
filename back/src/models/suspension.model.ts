90d3c36afb5000118b2aa5b8cc083b774208d28d
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2023-12-17 16:54:01 +0100

    ajout de models pour le front

---
+++ back/src/models/suspension.model.ts
@@ -0,0 +1,5 @@
+export interface ISuspension {
+    message_utilisateur: string | null,
+    date_debut: Date,
+    date_fin: Date | null
+};
---
+++ back/src/models/utilisateur.model.ts
@@ -0,0 +1,7 @@
+export interface IUtilisateur {
+    pk_utilisateur_id: number,
+    nom: string,
+    prenom: string,
+    genre: string | null,
+    email: string
+};
