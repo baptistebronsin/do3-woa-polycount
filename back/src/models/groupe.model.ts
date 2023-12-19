27966aa1cb319c629500e8ce72c73c30ca91681d
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2023-12-19 20:32:59 +0100

    ajout du modèle

---
+++ back/src/models/groupe.model.ts
@@ -0,0 +1,9 @@
+export interface IModel {
+    pk_groupe_id: number,
+    nom: string | null,
+    description: string | null,
+    fk_utilisateur_createur_id: number,
+    cree_le: Date,
+    cloture_le: Date | null,
+    lien_image: string | null
+};
